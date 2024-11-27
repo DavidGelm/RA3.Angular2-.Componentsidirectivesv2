51f637e30ff7440ac707db6bbd19450a68080be1
DavidGelm <david.gelma@institutvidreres.cat>
2024-11-27 13:33:34 +0100

    Exercici 2 Angular

--- src/app/components/correu/correu.component.ts
+++ src/app/components/correu/correu.component.ts
@@ -9,7 +9,9 @@ import {Component, OnInit} from '@angular/core';
 export class CorreuComponent implements OnInit{
 
   titol: string = 'David';
-
+  titolLength(): number {
+    return this.titol.length;
+  }
 
   constructor() {}
 
