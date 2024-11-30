f39cbf623ecdb162fdcfd6af4e39bbfc19124537
DavidGelm <david.gelma@institutvidreres.cat>
2024-11-30 16:42:06 +0100

    Exercici 4 Angular

--- src/app/components/llista-correus/llista-correus.component.ts
+++ src/app/components/llista-correus/llista-correus.component.ts
@@ -1,9 +1,9 @@
 import {Component, OnInit} from '@angular/core';
-import {NgForOf} from '@angular/common';
+import {NgClass, NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-llista-correus',
-  imports: [NgForOf],
+  imports: [NgForOf, NgClass],
   templateUrl: './llista-correus.component.html',
   standalone: true,
   styleUrl: './llista-correus.component.css'
@@ -12,8 +12,8 @@ export class LlistaCorreusComponent implements OnInit {
   titol: string = 'Correus amb ngFor'
   titol2: string = "Correus sense ngFor"
   correus =
-    [{titol: 'Correu 1',contingut:'Primer correu',correu:'pere@gmail.com'},
-      {titol:'Correu 2',contingut:'Segon correu',correu:'maria@gmail.com'}
+    [{titol: 'Correu 1',contingut:'Primer correu',correu:'pere@gmail.com',llegit:true},
+      {titol:'Correu 2',contingut:'Segon correu',correu:'maria@gmail.com',llegit:false}
     ];
   constructor() {}
   ngOnInit() {}
